d2b4d6872ad501de1a6508ee9c918e2d2868af79
uixPhuke <uixphuke@gmail.com>
2025-10-20 07:23:19 +0530

    Mongo DB setup

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
 //Import Express
 const express = require('express');
-
-//Create an Express app
 const app = express();
+
+const connectDB = require('./config/db')
+const PORT=process.env.PORT
 
 //testing route
 app.get('/', (req, res) => {
   res.send('I Camp Server');
 });
 
+//connect to database
+connectDB()
+
 //Start the server
-const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
